7d69e1e3f3d9f66b9c4b6da4c9a63a8ad5863e67
yenilikci <melihcelik017@outlook.com>
2020-11-08 22:03:58 +0300

    Story eklendi

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import styled from 'styled-components/native';
 import AppBar from "./components/AppBar";
 import ToolBar from "./components/ToolBar";
 import Users from "./components/Users";
+import Story from "./components/Story";
 
 const Container = styled.SafeAreaView`
   flex:1;
@@ -21,6 +22,7 @@ const App = () => {
             <AppBar/>
             <ToolBar/>
             <Users/>
+            <Story/>
         </ScrollView>
       </Container>
       </>
--- components/Avatar.js
+++ components/Avatar.js
@@ -12,6 +12,8 @@ const User = styled.Image`
     width: 40px;
     height: 40px;
     border-radius: 20px;
+    border-color: #1777f2;
+    border-width: ${props => (props.story ? "3px" : 0)};
 `
 
 const UserActive = styled.View`
@@ -26,10 +28,10 @@ const UserActive = styled.View`
     border-color: #ffffff;
 `
 
-const Avatar = ({source,online}) => {
+const Avatar = ({source,online,story}) => {
     return(
         <Container>
-            <User source={source}/>
+            <User source={source} story={story}/>
             {online && <UserActive/>}
         </Container>
     )
